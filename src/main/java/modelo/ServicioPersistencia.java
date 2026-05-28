ec40a7ba20e6b4685ac7759b23afae0b18b07084
Juan José Sánchez <jjsanchezcachero@outlook.com>
2026-05-28 12:18:13 +0200

    Eliminar Warning

--- src/main/java/modelo/ServicioPersistencia.java
+++ src/main/java/modelo/ServicioPersistencia.java
@@ -8,7 +8,7 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
+
 
 /**
  * Gestiona la persistencia del estado del sistema en formato JSON.
